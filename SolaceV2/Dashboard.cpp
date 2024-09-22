6ec5806e7ff3f3c160564bfcfc3d5348aa77e478
XeCrippy <mevans407.me@gmail.com>
2024-09-22 08:04:33 -0400

    Delete SolaceV2/Dashboard.cpp
    Not needed

--- SolaceV2/Dashboard.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "Dashboard.h"
-namespace SolaceV2 {
-    void Dashboard::Init()
-    {
-        utils::XNotify("xSolace 360 : Dashboard Loaded. Press LB+RB to view info");
-    }
-}
